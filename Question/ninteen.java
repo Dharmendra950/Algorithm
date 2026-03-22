1786e6c3d831d13f4c076a6c022dc97d7d1421f3
Dharmendra950 <mehtadharmendrakumar3@gmail.com>
2026-03-22 18:51:23 +0530

    Add files via upload

---
+++ Question/ninteen.java
@@ -0,0 +1,42 @@
+package Question;
+
+public class ninteen {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+/*
+    what is returned for a = 4,b = 3 ?
+    
+    Integer computeValue(Integer a , Integer b)
+            Integer result = 1 
+            while(b>0)
+                if(b mod 2 EQUALS 1)
+                result = result*a
+                a = a*a
+                b = b/2
+            End while
+            return result
+    End function
+    
+    a.64 correct
+    b.81
+    c.16
+    d.32
+
+    what will be the output of the following pseudo code for a = 2 ,b=6 ,c = 1
+
+    Integer computeValue(Integer a , Integer b , Integer)
+            if((c^b+a)<(a&c))
+                b=a^b (^ means XOR)
+            c = (a+3)+c
+        End if
+        return a + b + c
+
+        a.11
+        b.16
+        c.28
+        d.18
+    */
